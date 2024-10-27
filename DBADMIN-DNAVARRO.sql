5829467b7e2eceead77977974c6aebbb270c2e25
DENR1908 <167726273+DENR1908@users.noreply.github.com>
2024-10-27 14:38:47 -0300

    RedMedical.sql

--- DBADMIN-DNAVARRO.sql
+++ DBADMIN-DNAVARRO.sql
@@ -1,51 +1,106 @@
-CREATE SCHEMA db_adm;
-CREATE TABLE id_user (
-id INT PRIMARY KEY,
-FName varchar (40),
-LName varchar (50),
-user_ varchar(10));
-
-CREATE TABLE email_user(
-id INT PRIMARY KEY,
-email_user_ varchar(100));
-
-CREATE TABLE user_role(
-id INT PRIMARY KEY,
-role_ varchar(10));
+create database RedMedical;
+use Redmedical;
 
 
-INSERT INTO id_user VALUES 
-(1,"Federico", "Gaspari", "FGASPARI"),
-(2,"Soledad", "Grondona", "SGRONDONA"),
-(3,"Miguel", "Quispe", "MQUISPE"),
-(4,"Valentina", "Perez", "VPEREZ"),
-(5,"Ramiro", "Lezama", "RLEZAMA"),
-(6,"Antonio", "Fermin", "AFERMIN"),
-(7,"Ulises", "Lopez", "ULOPEZ"),
-(8,"Maria", "Carmona", "MCARMONA");
+create table Pacientes (
+	PxID INT NOT NULL auto_increment,
+    Name varchar (25)not null,
+    Apellido varchar (30) not null,
+    Dirección varchar (100) not null,
+    Edad varchar (3),
+	DNI INT NOT NULL,
+    PRIMARY KEY (PxID)
+);
+
+INSERT INTO pacientes (Name, Apellido, Dirección, DNI) VALUES
+('Juan', 'Pérez', 'Calle Falsa 123, Ciudad', 12345678),
+('María', 'Gómez', 'Avenida Siempre Viva 456, Ciudad', 23456789),
+('Luis', 'Martínez', 'Calle de la Paz 789, Ciudad', 34567890);
+
+UPDATE pacientes 
+SET edad = 23 
+where PxId = 1;
+
+UPDATE pacientes 
+SET edad = 25
+where PxId = 3;
+
+UPDATE pacientes 
+SET edad = 30
+where PxId = 2;
+
+SELECT * FROM Pacientes;
+    
 
 
-SELECT * FROM id_user;
+    
+create table Cobertura(
+	CoberturaID INT NOT NULL auto_increment,
+    PxDNI INT NOT NULL,
+    Empresa varchar (25) not null,
+    Plan varchar (15) not null,
+    PRIMARY KEY (CoberturaID),
+    foreign key (PxDNI)references pacientes (PxID)
+    
+);
 
-INSERT INTO email_user values 
-(1,"FGASPARI@ROWING.COM.AR"),
-(2,"SGRONDONA@ROWING.COM.AR"),
-(3,"MQUISPE@ROWING.COM.AR"),
-(4,"VPEREZ@ROWING.COM.AR"),
-(5,"RLEZAMA@ROWING.COM.AR"),
-(6,"AFERMIN@ROWING.COM.AR"),
-(7,"ULOPEZ@ROWING.COM.AR"),
-(8,"MCARMONA@ROWING.COM.AR");
-
-SELECT * FROM email_user;
+ALTER TABLE COBERTURA ADD DNI INT NOT NULL;
+ALTER TABLE PACIENTES ADD INDEX idx_px (PxID asc);
 
 
-INSERT INTO user_role values 
-(1, "ADMIN"),
-(2, "ADMIN"),
-(3, "ADMIN"),
-(4, "NORMAL"),
-(5,"ADMIN"),
-(6,"PRO"),
-(7,"PRO"),
-(8,"PRO");
+INSERT INTO Cobertura (PxDNI, Empresa, Plan)VALUES
+(1,"SWISS MEDICAL", "SMG20"),
+(3,"OSDE", "210"),
+(2,"MEDICUS", "PLAN CELESTE");
+
+
+
+
+
+
+
+CREATE TABLE Diagnosticos (
+	DxID INT NOT NULL AUTO_INCREMENT,
+    Diagnostico VARCHAR (30),
+    PRIMARY KEY (DxID)
+);
+
+
+INSERT INTO Diagnosticos (Diagnostico) VALUES
+("ACIDOSIS LACTICA TIPO B"),
+("PANCREATITIS"),
+("ESCLEROSIS MULTIPLE"),
+("ARTRITIS"),
+("GASTROENTERITIS");
+
+SELECT * FROM DIAGNOSTICOS;
+
+
+
+CREATE TABLE TRATAMIENTOS (
+	TratamientosID INT NOT NULL AUTO_INCREMENT,
+    PxID INT NOT NULL,
+	DxID INT NOT NULL,
+    Tratamiento varchar (30),
+    PRIMARY KEY (TratamientosID),
+    FOREIGN KEY (PxID) references pacientes (PxID),
+    FOREIGN KEY (DxID) references diagnosticos (DxID)
+);
+
+
+INSERT INTO Tratamientos (PxID, DxID, Tratamiento) Values 
+(1,2, "Inyeccion de Fosforo"),
+(2,5, "Dieta Liquida y Solucion Salina"),
+(3,4, "Solo administrar analgesicos");
+
+ALTER TABLE Tratamientos
+MODIFY COLUMN Tratamiento varchar (50);
+
+
+SELECT * FROM COBERTURA;
+SELECT * FROM DIAGNOSTICOS;
+SELECT * FROM PACIENTES;
+SELECT * FROM TRATAMIENTOS ORDER BY DxID asc;
+
+
+select t.PxID as PacienteID, t.Tratamiento as Tratamiento, p.Name as Nombre  from tratamientos t inner join pacientes p on t.PxID = p.PxId;
